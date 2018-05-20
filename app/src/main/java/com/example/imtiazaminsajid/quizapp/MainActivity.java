c582db650ebda2f6c3441ed6220bba3d340759c1
Imtiaz Amin Sajid <imtiazaminsajid@gmail.com>
2018-05-20 22:45:17 +0600

    Coding

--- app/src/main/java/com/example/imtiazaminsajid/quizapp/MainActivity.java
+++ app/src/main/java/com/example/imtiazaminsajid/quizapp/MainActivity.java
@@ -1,13 +1,51 @@
 package com.example.imtiazaminsajid.quizapp;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    MaterialEditText edtNewUser, edtNewPassword, edtNewEmail; //Sign up
+    MaterialEditText edtUser, edtPassword; //Sign in
+
+    Button btnSignUp, btnSignIn;
+
+    FirebaseDatabase database;
+    DatabaseReference users;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        database = FirebaseDatabase.getInstance();
+        users = database.getReference("Users");
+
+        edtUser = (MaterialEditText) findViewById(R.id.edtUser);
+        edtPassword = (MaterialEditText) findViewById(R.id.edtPassword);
+
+        btnSignIn = findViewById(R.id.btn_sign_in);
+        btnSignUp = findViewById(R.id.btn_sign_up);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSignUpDialog();
+            }
+        });
+    }
+
+    private void showSignUpDialog() {
+
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+        alertDialog.setTitle("Sign Up");
+        alertDialog.setMessage("Please Full Fill all information");
     }
 }
