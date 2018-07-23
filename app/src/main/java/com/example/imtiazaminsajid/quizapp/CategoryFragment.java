2a7afa8981901ddfbc80bc2d12646f96f70aba09
Imtiaz Amin Sajid <imtiazaminsajid@gmail.com>
2018-07-23 23:55:14 +0600

    Home

---
+++ app/src/main/java/com/example/imtiazaminsajid/quizapp/CategoryFragment.java
@@ -0,0 +1,23 @@
+package com.example.imtiazaminsajid.quizapp;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CategoryFragment extends Fragment {
+
+    public static CategoryFragment newInstance() {
+        CategoryFragment categoryFragment = new CategoryFragment();
+        return categoryFragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
